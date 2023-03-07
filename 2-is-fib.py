d857b99958796ca8be222dce438ae8d79e165f5d
wjld <98484634+wjld@users.noreply.github.com>
2023-03-07 10:47:30 -0300

    solve question 2

---
+++ 2-is-fib.py
@@ -0,0 +1,14 @@
+'''n is a fibonacci number if and only if one of
+5*n**2 ± 4 is a perfect square'''
+
+def isPerfectSquare(n):
+    return n**0.5 == int(n**0.5)
+ 
+def isFibonacci(n):
+    return isPerfectSquare(5*n**2 + 4) or isPerfectSquare(5*n**2 - 4)
+
+n = input("insert a number: ")
+if (isFibonacci(n)):
+    print (n,"is a fibonacci number")
+else:
+    print (n,"is a not fibonacci number ")
