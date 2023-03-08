124858a1fdd8b0b1cb288ff7f08657d877252e8d
wjld <98484634+wjld@users.noreply.github.com>
2023-03-08 13:45:00 -0300

    fix calling function with string instead of int

--- 2-is-fib.py
+++ 2-is-fib.py
@@ -8,7 +8,7 @@ def isFibonacci(n):
     return isPerfectSquare(5*n**2 + 4) or isPerfectSquare(5*n**2 - 4)
 
 n = input("insert a number: ")
-if (isFibonacci(n)):
+if (isFibonacci(int(n))):
     print (n,"is a fibonacci number")
 else:
     print (n,"is a not fibonacci number ")
